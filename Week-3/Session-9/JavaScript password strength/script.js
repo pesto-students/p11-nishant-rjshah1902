60c954be39e736c53099189db3dc237743f1c563
Rahul shah <66217863+rjshah1902@users.noreply.github.com>
2023-05-05 22:23:54 +0530

    Password Check

--- Week-3/Session-9/JavaScript password strength/script.js
+++ Week-3/Session-9/JavaScript password strength/script.js
@@ -1,27 +1,32 @@
-
 isStrongPassword();
-
-
 
 function isStrongPassword(){
 
-    var password = prompt('Enter your Password');
+    let password = prompt('Enter your Password');
 
-    if(password.length < 8){
-    
-        alert('Password is Very Short, Use Atleast 8 Character');
-    
-    } else if(password.length > 8){
-    
-        if(!password.match('/[a-z]/')) {
+    if(password.length > 8){
+
+        if(Number.isInteger(password) !== password) {
         
-            alert('Please Enter Small Letter');
+            password = prompt('Password is very easy, please Use Atleast 1 Number Character');
         
+        }else if(password.toLowerCase() !== password) {
+        
+            password = prompt('Password is very easy, please Use Atleast 1 lowercase Character');
+        
+        } else if(password.toUpperCase() !== password) {
+        
+            password = prompt('Password is very easy, please Use Atleast 1 uppercase Character');
+            
         } else {
 
             alert('Your Password is Good');
 
         }
+    
+    } else if(password.length < 8){
+    
+        password = prompt('Password is Very Short, Use Atleast 8 Character');
     
     } else {
     
