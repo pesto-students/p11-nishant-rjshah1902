955669c88d0168fc3d0ce0f36e03c7083a54903c
Rahul shah <66217863+rjshah1902@users.noreply.github.com>
2023-05-09 10:44:40 +0530

    Javascript array done

--- Week-3/Session-9/JavaScript arrays/script.js
+++ Week-3/Session-9/JavaScript arrays/script.js
@@ -1,10 +1,26 @@
 
 let nums = [4,2,9,1,8];
 
-let oddnumbers = [2, 4, 8];
+let oddnumbers = [];
 
-let evennumbers = [1,9];
+let evennumbers = [];
 
 function divideArray(nums){
+    
+    for(i=0; i<nums.length; i++){
 
+        if(nums[i]%2 === 0){
+            oddnumbers.push(nums[i]);
+        }else{
+            evennumbers.push(nums[i]);
+        }
+   }
+   
+   return { oddnumbers: oddnumbers, evennumbers: evennumbers }
 }
+
+var response = divideArray(nums);
+
+console.log(response.oddnumbers.sort());
+
+console.log(response.evennumbers.sort());
